74f502e569f69e6ab9625841d8e0c2d186b21026
Camila Polo <camilasp@gmail.com>
2023-03-05 22:48:33 -0300

    adicionando página de visão detalhada de filme

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+document.addEventListener( "click", function toggleWords ( event ) {
+    // A few variables to help us track important values/references
+    var target = event.target, values = [], placed;
+    // If the clicked element has multiple values
+    if ( target.hasAttribute( "data-values" ) ) {
+        // Split those values out into an array
+        values = target.getAttribute( "data-values" ).split( "," );
+        // Find the location of its current value in the array
+        placed = values.indexOf( target.textContent );
+        // Set its text to be the next value in the array
+        target.textContent = values[ ++placed % values.length ];   
+    }
+});
