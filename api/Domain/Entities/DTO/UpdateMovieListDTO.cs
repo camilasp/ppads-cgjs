22f478217486243bd3ddec5c1df03dc4ac258280
João Victor de Souza <j.souzalimasantos@gmail.com>
2023-05-05 19:57:23 -0300

    Delete api directory

--- api/Domain/Entities/DTO/UpdateMovieListDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Entities.DTO
-{
-    public sealed class UpdateMovieListDTO
-    {
-        public Guid UserId { get; set; }
-        public bool Favorite { get; set; }
-        public int MovieId { get; set; }
-    }
-}
