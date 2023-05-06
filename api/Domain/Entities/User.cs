af5b3c9190197bde6736f7b6b0d9e55a784dc09c
João Victor de Souza <j.souzalimasantos@gmail.com>
2023-05-06 11:37:09 -0300

    feat: create the models 'User' and 'MovieReference'

---
+++ api/Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public sealed class User
+    {
+        [Key]
+        public Guid Id { get; set; } = new Guid();
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        public ICollection<MovieReference> MovieReferences { get; set; }
+    }
+}
