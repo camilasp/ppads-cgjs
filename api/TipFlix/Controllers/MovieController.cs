74c3a4b2d2c6c88b97c6b8c131e9dc584b6548a2
João Victor de Souza <j.souzalimasantos@gmail.com>
2023-05-06 11:47:50 -0300

    feat: create the controllers for 'User' and 'Movie'

---
+++ api/TipFlix/Controllers/MovieController.cs
@@ -0,0 +1,46 @@
+﻿using Application.Interfaces;
+using Domain.Entities.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TipFlix.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public sealed class MovieController : Controller
+    {
+        private readonly IHandler _handler;
+
+        public MovieController(IHandler handler)
+        {
+            _handler = handler;
+        }
+
+        [HttpGet("PopularMovies")]
+        public async Task<ActionResult<ICollection<MinimalMovieDTO>>> Get()
+        {
+            try
+            {
+                var moviesInfo = _handler.GetPopularMoviesAsync();
+                return Ok(moviesInfo);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+        }
+
+        [HttpGet("MovieInfo")]
+        public async Task<ActionResult<MovieFavDTO>> Get([FromQuery] Guid guid, int id)
+        {
+            try
+            {
+                var movie = await _handler.GetMovieByIdAsync(guid, id);
+                return Ok(movie);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ api/TipFlix/Controllers/UsersController.cs
@@ -0,0 +1,82 @@
+﻿using Application.Interfaces;
+using Domain.Entities.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TipFlix.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public sealed class UsersController : Controller
+    {
+        private readonly IHandler _handler;
+
+        public UsersController(IHandler handler)
+        {
+            _handler = handler;
+        }
+
+        [HttpGet("Login")]
+        public async Task<ActionResult<UserDTO>> LoginAsync([FromQuery] string email, string password)
+        {
+            try
+            {
+                var userDto = await _handler.LoginUserAsync(email, password);
+                return userDto is not null ? (ActionResult<UserDTO>)Ok(userDto) : (ActionResult<UserDTO>)NotFound("Usuário não cadastrado.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+        }
+
+        [HttpPost("SignIn")]
+        public async Task<ActionResult> PostAsync([FromBody] UserDTO userDTO)
+        {
+            try
+            {
+                if (userDTO is not null)
+                {
+                    await _handler.CreateUserAsync(userDTO);
+                    return Ok("Registrado novo usuário.");
+                }
+                else
+                {
+                    return BadRequest("Envio objeto nulo.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+        }
+
+        [HttpPut("UpdateMovieList")]
+        public async Task<ActionResult> UpdateMovieListAsync([FromBody] UpdateMovieListDTO updateMovieListDTO)
+        {
+            try
+            {
+                await _handler.UpdateUserMovieListAsync(updateMovieListDTO);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<ActionResult> DeleteAsync([FromQuery] Guid guid)
+        {
+            try
+            {
+                await _handler.DeleteUserAsync(guid);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+        }
+    }
+}
