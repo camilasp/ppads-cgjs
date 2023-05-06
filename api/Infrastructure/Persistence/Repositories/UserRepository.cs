9301639c434808eb8d3272bc8f94d92b6ea97ceb
João Victor de Souza <j.souzalimasantos@gmail.com>
2023-05-06 11:43:42 -0300

    feat: implement the repositories for the sln database

---
+++ api/Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+using Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public sealed class UserRepository : Repository<User>, IUserRepository
+    {
+        public UserRepository(AppDbContext context) : base(context) { }
+
+        public async Task<bool> HasUserWithEmailAsync(string email)
+        {
+            try
+            {
+                var user = await Get()
+                    .FirstOrDefaultAsync(user => user.Email == email);
+
+                return user != null;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<User> CheckUserCredentialsAsync(string email, string password)
+        {
+            try
+            {
+                var user = await Get()
+                    .FirstOrDefaultAsync(user => 
+                    user.Email == email && user.Password == password);
+
+                return user;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
