cbe78ac2eabffeb614108f1b43e6a7fbf327a753
João Victor de Souza <j.souzalimasantos@gmail.com>
2023-05-06 11:43:20 -0300

    feat: create the database context class, to work with EF Core

---
+++ api/Infrastructure/Persistence/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Context
+{
+    public sealed class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<MovieReference> MovieReferences { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasMany(x => x.MovieReferences)
+                .WithOne(x => x.User)
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
